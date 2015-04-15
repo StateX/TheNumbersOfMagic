f1cb62511ba860a24d9d379a96fb68b241c3a536
Albert Sanchez Carbonell <statexblog@gmail.com>
2015-04-15 20:48:07 +0200

    The Numbers Of Magics v0.1
    Module for files whithout extension

---
+++ TheNumbersOfMagic.py
@@ -0,0 +1,35 @@
+import binascii
+import re 
+
+def octarine(spectrum):
+    magicclothes = {"25504446":"PDF Document or Adobe Portable Document Format and Forms Document file", "424d":"Bitmap graphic","ffd8":"JPEG graphic file",
+                 "cafebabe":"Class File", "0000000c6a5020200D0a": "JPEG 2000","47494638":"GIF graphic file", "4949":"TIF graphic file",
+                 "89504e470d0a1a0a":"PNG graphic file","38425053":"Photoshop Graphics","d7cdc69a":"Windows Meta File","4d546864":"MIDI file","00000100":"Icon file",
+                 "494433":"MP3 file with ID3 identity tag","52494646":"AVI video file","465753":"Flash Shockwave","464c56 ":"Flash Video",
+                 "00000018667479706d703432":"Mpeg 4 video file","6d6f6f76":"MOV video file","3026b2758e66cf":"Windows Video file",
+                 "3026b2758e66cf":"Windows Audio file","504b0304":"PKZip","1f8b08":"GZip","7573746172":"Tar file","d0cf11e0a1b11ar1":"Microsoft Installer",
+                 "4c01":"Object Code File","4d5a":"Dynamic Library","4d534346":"CAB Installer file","4D 5A":"Executable file","RAR file":"526172211a0700",
+                 "4d5a":"SYS file","3f5f0300":"Help file","4b444d56":"VMWare Disk file","2142444e42":"Outlook Post Office file",
+                 "d0cf11e0a1b11ae1":"Word Document or Excel Document or PowerPoint Document or Visio Document or Outlook Message File","7b5c72746631":"RTF Document",
+                 "504b0304":"DOCX (Office 2010) or XLSX (Office 2010) or PPTX (Office 2010)",
+                 "5374616e64617264204a6574":"Microsoft Database","2521":"Postcript File","252150532d41646f62652d332e3020455053462d332030":"EPS File",
+                 "504b0304140008000800":"Jar File","4d6963726f736f66742056697375616c2053747564696f20536f6c7574696f6e2046696c65":"SLN File",}
+
+
+    
+    with open(spectrum,'rb') as m:
+        magic = m.read(36)
+
+    print (magic)
+
+    d= str(binascii.hexlify(magic))
+    print ("The magic number is:",d)
+    for terry in magicclothes:
+        o1 = re.findall(terry,d)
+        if o1:
+            print ("It can be a ", magicclothes[terry])
+
+if __name__ == "__main__":
+    
+    file = input("Be careful with the number 8: ")
+    octarine(archivo.strip('"'))
---
+++ setup.py
@@ -0,0 +1,4 @@
+from distutils.core import setup
+setup (name = "The Numbers Of Magic",
+       version = "0.1",
+       py_modules = ["TheNumbersOfMagic"])
